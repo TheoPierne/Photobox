3a6ca389dfafb071d0639609529f30233e748fd9
winvdaking <pro.dorianlopez@gmail.com>
2022-10-26 15:36:43 +0200

    chore(gallery): from let to const URI

--- modules/gallery.js
+++ modules/gallery.js
@@ -3,7 +3,7 @@ import { loadResource } from "./api.js";
 export let lesPhotos = [];
 
 export async function load(){
-    let uri = '/www/canals5/phox/api/photos';
+    const uri = '/www/canals5/phox/api/photos';
     const data = await loadResource(uri);
     lesPhotos = lesPhotos.concat(data);
     return data.photos;
