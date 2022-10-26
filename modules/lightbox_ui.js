da2b7dca2b79225f6a783498e952070b92e2b1dd
TheoPierne <theo.pierne2@gmail.com>
2022-10-26 17:25:13 +0200

    feat: added full display_lightbox

--- modules/lightbox_ui.js
+++ modules/lightbox_ui.js
@@ -1,8 +1,17 @@
+import { BASE_ENDPOINT } from './constants.js';
+import { lesPhotos } from './gallery.js';
 
 const $lightbox_container = document.getElementById('lightbox_container');
+const $lightbox_full_img = document.getElementById('lightbox_full_img');
+const $lightbox_title = document.getElementById('lightbox_title');
 
-export function display_lightbox(data){
-
+export function display_lightbox(uri){
+	const data = lesPhotos.find(e => e.links.self.uri === uri);
+	if (data) {
+		$lightbox_full_img.src = `${BASE_ENDPOINT}${data.photo.original.href}`;
+		$lightbox_title.innerText = data.photo.titre;
+		show();
+	}
 }
 
 export function show(){
