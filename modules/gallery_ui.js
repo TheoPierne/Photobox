d6b4be059571390b80428782c8e1a2ec0f7c3fa1
TheoPierne <theo.pierne2@gmail.com>
2022-10-26 16:36:59 +0200

    hotfix: removed default from export default

--- modules/gallery_ui.js
+++ modules/gallery_ui.js
@@ -11,7 +11,7 @@ const $gallery_container = document.getElementById('gallery_container');
  * Fonction permettant d'afficher une galerie à partir des données de l'API.
  * @param {Array} gallery Les données de l'API représentées sous forme d'un tableau d'object. 
  */
-export default function display_gallerie(gallery = []){
+export function display_gallerie(gallery = []){
 	gallery.forEach(e => {
 		const { thumbnail: { href: uri } } = e.photo;
 		const { self: { href: src } } = e.links;
