1fd2b36a6e6ec81dc4e1c4501ff8feb82c347a2e
winvdaking <pro.dorianlopez@gmail.com>
2022-10-26 16:10:44 +0200

    chore(lightbox): add comments

--- modules/lightbox.js
+++ modules/lightbox.js
@@ -1,5 +1,10 @@
 import { loadResource } from "./api.js";
 
+/**
+ * La fonction retourne un objet photo à partir de l'URI passé dans un dataset dans le noeux en paramètre.
+ * @param {Node} node - Noeux de l'arborescence DOM.
+ * @returns {Photo} - Object de type Photo retourné par l'API.
+ */
 export async function load(node){
     const data = await loadResource(node.dataset.uri);
     return data;
