7d1273c9c4b2602d44529b1542bfa50b9f9d6c1d
winvdaking <pro.dorianlopez@gmail.com>
2022-11-24 10:24:11 +0100

    feat(lightbox_ui): add frm comment & post comment

--- modules/lightbox_ui.js
+++ modules/lightbox_ui.js
@@ -1,5 +1,5 @@
 import { loadResource } from './api.js';
-import { BASE_ENDPOINT } from './constants.js';
+import { API_ENDPOINT, BASE_ENDPOINT } from './constants.js';
 import { lesPhotos } from './gallery.js';
 
 const $lightbox_container = document.getElementById('lightbox_container');
@@ -7,6 +7,10 @@ const $lightbox_full_img = document.getElementById('lightbox_full_img');
 const $lightbox_title = document.getElementById('lightbox_title');
 const $lightbox_close = document.getElementById('lightbox_close');
 const $espace_commentaires = document.getElementById('commentaires');
+const $id_photo = document.getElementById('id_photo');
+const $pseudo = document.getElementById('pseudo');
+const $titre = document.getElementById('titre');
+const $content = document.getElementById('content');
 
 $lightbox_close.onclick = () => {
 	hide();
@@ -19,6 +23,7 @@ export async function display_lightbox(uri) {
 	if (data) {
 		$lightbox_full_img.src = `${BASE_ENDPOINT}${data.photo.original.href}`;
 		$lightbox_title.innerText = data.photo.titre;
+		$id_photo.value = data.photo.id;
 		commentaires = await loadResource(uri + '/comments');
 		if (commentaires.comments) {
 			let $comments = [];
@@ -46,4 +51,16 @@ function displayComment(comment) {
 		<span>De ${comment.pseudo}</span>
 		<p>${comment.content}</p>
   	</div>`;
+}
+
+export async function ajouterCommentaire() {
+	await fetch(`${API_ENDPOINT}/photos/${$id_photo.value}/comments`, {
+		method: 'POST',
+		credentials: 'include',
+		body: JSON.stringify({
+			titre: $titre.value,
+			content: $content.value,
+			pseudo: $pseudo.value
+		})
+	});
 }
--- modules/photobox.js
+++ modules/photobox.js
@@ -1,23 +1,31 @@
 import { display_gallerie } from './gallery_ui.js';
 import { load, prev, next } from './gallery.js';
+import { ajouterCommentaire } from './lightbox_ui.js';
 
 const $load_gallery = document.getElementById('load_gallery');
 const $next = document.getElementById('next');
 const $previous = document.getElementById('previous');
+const $btnSubmit = document.getElementById('btn-submit');
 
 $load_gallery.onclick = async e => {
-  const data = await load();
-  display_gallerie(data);
+  init();
 };
 
 $next.onclick = async e => {
   next();
-  const data = await load();
-  display_gallerie(data);
+  init();
 }
 
 $previous.onclick = async e => {
   prev();
+  init();
+}
+
+(async function init() {
   const data = await load();
   display_gallerie(data);
+})();
+
+$btnSubmit.onclick = () => {
+  ajouterCommentaire();
 }
